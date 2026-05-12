65b7b62040f152d0edb1952a1640745936c0904e
sangyeop0529 <sangyeop0529@gmail.com>
2026-05-12 15:41:01 +0900

    feat: Tailwind CSS 주요 기능 테스트 및 예제 코드 작성

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,26 @@ import "./App.css";
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold text-amber-500 underline">
-      Hello world!
-    </h1>
+    <div>
+      {/* 타이포그래피 */}
+      <div className="text-xs text-red-500">text-xs</div>
+      <div className="text-sm text-[rgb(100,30,200)]">text-sm</div>
+      <div className="text-lg font-bold">text-lg</div>
+      <div className="text-xl font-extrabold">text-xl</div>
+      <div className="text-2xl font-black">text-2xl</div>
+      <div className="text-[13px]">text-13px</div>
+
+      {/* 백그라운드 */}
+      <div className="bg-amber-500">amber-500</div>
+
+      {/* 사이즈 */}
+      <div className="w-full bg-blue-500">box</div>
+
+      {/* 여백 */}
+      <div className="mx-5 my-5 h-50 w-50 bg-red-400 px-5 py-5">
+        <div className="h-full w-full bg-blue-400"></div>
+      </div>
+    </div>
   );
 }
 
