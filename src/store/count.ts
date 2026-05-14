1ab3244b7f3e84286a613af377ee87bd9674eadb
sangyeop0529 <sangyeop0529@gmail.com>
2026-05-14 13:01:43 +0900

    Zustand 개념정리 끝

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,24 +1,57 @@
 import { create } from "zustand";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
 
-type Store = {
-  count: number;
-  actions: {
-    increase: () => void;
-    decrease: () => void;
-  };
-};
+export const useCountStore = create(
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increase: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decrease: () => {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+    {
+      name: "countStore",
+    },
+  ),
+);
 
-export const useCountStore = create<Store>((set, get) => ({
-  count: 0,
-  actions: {
-    increase: () => {
-      set((store) => ({ count: store.count + 1 }));
-    },
-    decrease: () => {
-      set((store) => ({ count: store.count - 1 }));
-    },
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Listener
+    console.log(count, prevCount);
+
+    const store = useCountStore.getState();
   },
-}));
+);
 
 export const useCount = () => {
   const count = useCountStore((store) => store.count);
